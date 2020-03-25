717640bbb9eab4302e068c2f8c104913aedbff30
Hugo Marcelo <hugomarcelo91@gmail.com>
2020-03-25 17:29:24 -0300

    feat: create pages layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <Routes />;
 }
 
 export default App;
